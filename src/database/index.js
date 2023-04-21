c38baa4694a779b9d3fdb9082b28aecfbb3ba663
Gustavo Kei Amemiya <gustavokeia@gmail.com>
2023-04-21 15:33:09 -0300

    fix: fixing db conn config

--- src/database/index.js
+++ src/database/index.js
@@ -1,7 +1,7 @@
 const config = require("../config");
 const mysql = require("mysql2");
 
-const mysqlAccounts = mysql.createConnection(config.mysql);
-const mysqlUser = mysql.createConnection(config.mysql);
+const mysqlAccounts = mysql.createConnection(config.mysqlAccounts);
+const mysqlUser = mysql.createConnection(config.mysqlUser);
 
 module.exports = { mysqlAccounts, mysqlUser };
